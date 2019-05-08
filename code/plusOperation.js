ef376015a0bb7704d8c700366932e1bba7609a54
naye0ng <nayeong_e@naver.com>
2019-05-09 00:24:57 +0900

    더하기 연산 자바스크립트 생성 및 엔트포인트 생성(JS - models 연동)

---
+++ code/plusOperation.js
@@ -0,0 +1,15 @@
+// models/action/PlusOperation.model.bxb의 내용과 input, output의 변수명이 일치해야 한다.
+module.exports.function = function plusOperation (leftOperand, RightOperand) {
+  const console = require('console');
+  const result = leftOerand + RightOperand;
+  const operatorName = "더하기";
+  console.log("result is ",result);
+  
+  // models/concept/Results.model.bxb에서 정의한 property 이름과 일치해야 한다. 
+  const results = {
+    operatorName : operatorName,
+    result : operatorName
+  };
+  
+  return results;
+}
